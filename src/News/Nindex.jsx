cad51434000935c86071c63d71b6c2bdce5d17a7
Vivek Bhatt <bhattvivek889@gmail.com>
2022-08-02 00:26:17 +0530

    Completed

--- src/News/Nindex.jsx
+++ src/News/Nindex.jsx
@@ -4,12 +4,11 @@ import Card from './Card';
 function Nindex(){
     const[it,setIt]=useState("");
     const[Data,setData]=useState([]);
-    const[state,setState]=useState("Top Headlines");
+    const[state,setState]=useState("Trending");
     useEffect(()=>{
         async function getData(){
-            const res=await axios.get(`https://newsapi.org/v2/everything?q=${state}&pageSize=42&apiKey=ba2954c5cf594456b714ba894dce1275`)
+            const res=await axios.get(`https://newsapi.org/v2/everything?q=${state}&pageSize=54&apiKey=ba2954c5cf594456b714ba894dce1275`)
             setData(res.data.articles);
-            console.log(res.data);
         }
         getData();
     },[state]);
@@ -20,38 +19,42 @@ function Nindex(){
         setState(it);
         event.preventDefault();
     }
+    function handleit(event){
+        setState(event.target.value);
+    }
     return(
         <div>
             <nav className='Nindex-nav'>
-                <h1 className='Nindex-head'>E-NEWS</h1>
+                <h1 className='Nindex-head'><a className='Nindex-a' href='/'>E-NEWS</a></h1>
+                <select className='select' onChange={handleit} >
+                    <option className='option' selected disabled>Categories</option>
+                    <option className='option' value="Bollywood">Bollywood</option>
+                    <option className='option' value="Science">Science</option>
+                    <option className='option' value="Engineering">Engineering</option>
+                    <option className='option' value="Cars">Cars</option>
+                    <option className='option' value="Animals">Animals</option>
+                </select>
                 <form className="Nindex-form">
                     <input className='Nindex-input' value={it} onChange={handleChange} />
                     <button className='Nindex-btn' onClick={handleClick}>Search</button>
                 </form>
-                <div className='Nindex-div3'>
-                    <a className='Nindex-blog'>Blogs</a>
-                    <a className='Nindex-create'>Create Blog</a>
-                    <p className='Nindex-logout'>Logout</p>
-                </div>
             </nav>
             <h1 className='Nindex-headline'>{state}</h1>
-            <div className='Nindexdiv'>
-                {Data.map((x)=>{
-                    return <Card 
-                        id={x.id}
-                        title={x.title}
-                        description={x.description}
-                        url={x.url}
-                        image={x.urlToImage}
-                        publish={x.publishedAt}
-                        source={x.source.name}
-                    />
-                })}
-            </div>
+                <div className='Nindexdiv'>
+                    {Data.map((x)=>{
+                        return <Card 
+                            id={x.source.id}
+                            title={x.title}
+                            description={x.description}
+                            url={x.url}
+                            image={x.urlToImage}
+                            publish={x.publishedAt}
+                            source={x.source.name}
+                        />
+                    })}
+                </div>
+                <p className='Nindex-footer'>Made with &#10084;&#65039; by Vivek Bhatt</p>
         </div>
     );
 }
 export default Nindex;
-//f6b3ace87ee07ab01bbef5d7867bc479
-//d4d1cce4b3effe1ebab001b467afb29a
-//ba2954c5cf594456b714ba894dce1275
